74fc855c7ff2cba060b89eff8e52bf9885dc2c16
RayMoreira <91735324+RayMoreira@users.noreply.github.com>
2024-11-18 11:48:36 -0500

    Primeiro commit: Subindo projeto inicial

--- obj/Debug/net9.0/Atividade.AssemblyInfo.cs
+++ obj/Debug/net9.0/Atividade.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2af09f941d7781a20f08e10005c4fed9ca74c4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Atividade")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
